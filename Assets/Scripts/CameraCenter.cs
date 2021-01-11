39dfe63ed06cf91252eb7fb8c6170213ca063e75
MiraFinkel <mirafinkel@gmail.com>
2021-01-11 21:17:58 +0200

    levitation + centerOfCamera
    With object levitation and display of the centerOfCamera

---
+++ Assets/Scripts/CameraCenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraCenter : MonoBehaviour
+{
+    public TextMesh cameraCenter;
+    float radius = 3;
+
+    void Update()
+    {
+        cameraCenter.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane * 20));
+        cameraCenter.transform.rotation = Camera.main.transform.rotation;
+    }
+
+    public bool isInPlayersView(GameObject obj)
+    {
+        return Vector3.Distance(obj.transform.position, cameraCenter.transform.position) < radius;
+    }
+}
---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    CameraCenter cameraCenter;
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+    float amplitude = 0.2f;
+    float frequency = 1f;
+
+    void Start()
+    {
+        cameraCenter = GameObject.Find("CameraCenter").GetComponent<CameraCenter>();
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraCenter.isInPlayersView(gameObject))
+        {
+            tempPos = posOffset;
+            tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+            transform.position = tempPos;
+        }
+        else
+        {
+            transform.position = posOffset;
+        }
+        
+    }
+}
