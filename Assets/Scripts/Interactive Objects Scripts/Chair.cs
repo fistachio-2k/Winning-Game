bf5871d66db4ff2f9d3e9aaefdc03ad6c9bd0b65
DorGd <37707720+DorGd@users.noreply.github.com>
2021-01-16 18:26:38 +0200

    Chairrrrrrr

---
+++ Assets/Scripts/Interactive Objects Scripts/Chair.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class Chair : MonoBehaviour
+{
+    [SerializeField] private Transform positionToMove;
+    [SerializeField] private float duration = 2f;
+
+    void Update()
+    {
+        if (InputManager.Instance.GetTestButton1())
+        {
+            StartCoroutine(Sitting());
+        }
+    }
+
+    IEnumerator Sitting()
+    {
+        transform.DOMove(positionToMove.position, duration);
+        yield return new WaitForSeconds(duration);
+        GameEventsManager._instance.SwitchToVcam(GameEventsManager.Vcam.Sitting);
+    }
+}
