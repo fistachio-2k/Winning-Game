79d3fd3bb5c0895227e027ec8f53294e1c1aad6a
MiraFinkel <mirafinkel@gmail.com>
2021-01-16 11:02:36 +0200

    with outline
    with outline

---
+++ Assets/Scripts/IRayProvider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+public interface IRayProvider
+{
+    Ray CreateRay();
+}
+
+public class MouseScreenRayProvider : MonoBehaviour, IRayProvider
+{
+    public Ray CreateRay()
+    {
+        return Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane * 20));
+    }
+}
