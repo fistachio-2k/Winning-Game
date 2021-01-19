8c2916f82db887a553af69ee8040299cfa09b832
DorGd <37707720+DorGd@users.noreply.github.com>
2021-01-19 14:33:17 +0200

    Revert ". new"
    This reverts commit 06a53124c46b11962b25c5321ae8d802c6181c4c.

---
+++ Assets/Scripts/Interactive Objects Scripts/outline/ToggleOutline.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOutline : MonoBehaviour
+{
+    public CameraCenter cameraCenter;
+    void Start()
+    {
+        //cameraCenter = cameraCenter.GetComponent<CameraCenter>();
+        cameraCenter = FindObjectOfType<CameraCenter>().GetComponent<CameraCenter>();
+    }
+
+
+    void Update()
+    {
+        if (cameraCenter != null)
+        {
+            if (cameraCenter.isInPlayersView(gameObject))
+            {
+                transform.GetComponent<cakeslice.Outline>().EnableOutline();
+            }
+            else
+            {
+                transform.GetComponent<cakeslice.Outline>().DisableOutline();
+            }
+        }
+    }
+}
