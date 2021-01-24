b0d776d88c607f25afe06671c46e13b3fefc14ae
DorGd <37707720+DorGd@users.noreply.github.com>
2021-01-24 23:31:14 +0200

    Door logic + breakfast main door + Noa's UV and mat

--- Assets/Scripts/Interactive Objects Scripts/Door.cs
+++ Assets/Scripts/Interactive Objects Scripts/Door.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class Door : MonoBehaviour
+public class Door : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
+    private bool _isOpen = false;
+    [SerializeField] private bool _isLocked = false;
+    private Vector3 _baseRotation;
+    [SerializeField] float duration = 2f;
+
     void Start()
     {
-        
+        _baseRotation = transform.rotation.eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        StartCoroutine(OpenClose());
+    }
+
+    IEnumerator OpenClose()
+    {   
+        //TODO: Add locked / Open / Close sounds
+        if (!_isLocked)
+        {
+            _isOpen = !_isOpen;
+            if (_isOpen)
+            {
+                transform.DORotate(_baseRotation, duration);
+            }
+            else
+            {
+                transform.DORotate(_baseRotation + (Vector3.up * 90f) + (Vector3.forward * 3f) + (Vector3.right * -3f), duration);
+            }
+        }
+        yield return null;
     }
 }
