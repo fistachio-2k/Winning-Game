d3abbc869ad1d9f02273db510ea2f1ff67a8f0c9
MiraFinkel <mirafinkel@gmail.com>
2021-01-26 10:38:49 +0200

    with Third House corridor interactible objects
    with Third House corridor interactible objects

---
+++ Assets/Scripts/Interactive Objects Scripts/CookBook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookBook : MonoBehaviour, IInteractable
+{
+    [SerializeField] private TextReveal bookText;
+
+    public void Interact()
+    {
+        StartCoroutine(bookText.RevealText());
+    }
+}
