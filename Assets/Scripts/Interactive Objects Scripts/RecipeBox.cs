f6240790b8b317aadf2252bac5cbd49a7bdf2d1b
MiraFinkel <mirafinkel@gmail.com>
2021-02-02 15:47:56 +0200

    recipe
    recipe

--- Assets/Scripts/Interactive Objects Scripts/RecipeBox.cs
+++ Assets/Scripts/Interactive Objects Scripts/RecipeBox.cs
@@ -5,15 +5,20 @@ using DG.Tweening;
 
 public class RecipeBox : MonoBehaviour, IInteractable
 {
-    private bool _isOpen = true;
-    private Vector3 _baseRotation;
-    [SerializeField] private bool _isLocked = false;
+    [SerializeField] private GameObject boxLib;
+    [SerializeField] private GameObject boxBody;
+    [SerializeField] private GameObject recipe;
+    [SerializeField] public bool isLocked = true;
     [SerializeField] float duration = 2f;
+
+    private bool _isOpen = false;
+    private Vector3 _baseRotation;
+
     private AudioManager audioManager;
 
     void Start()
     {
-        _baseRotation = transform.rotation.eulerAngles;
+        _baseRotation = boxLib.transform.rotation.eulerAngles;
         audioManager = FindObjectOfType<AudioManager>();
     }
 
@@ -24,21 +29,26 @@ public class RecipeBox : MonoBehaviour, IInteractable
 
     IEnumerator OpenClose()
     {
-        if (!_isLocked)
+        if (!isLocked)
         {
-            _isOpen = !_isOpen;
-            if (_isOpen)
+            //_isOpen = !_isOpen;
+            //if (_isOpen)
+            //{
+            //    transform.DORotate(_baseRotation, duration);
+            //}
+            if (!_isOpen)
             {
-                transform.DORotate(_baseRotation, duration);
+                boxLib.transform.DORotate(_baseRotation + (Vector3.up * 90f) + (Vector3.forward * 3f) + (Vector3.right * -3f), duration);
+                gameObject.GetComponent<BoxCollider>().enabled = false;
+                recipe.AddComponent<Collect>();
             }
             else
             {
-                transform.DORotate(_baseRotation + (Vector3.up * 90f) + (Vector3.forward * 3f) + (Vector3.right * -3f), duration);
+                if(recipe != null)
+                {
+                    recipe.GetComponent<Collect>().Interact();
+                }
             }
-        }
-        else
-        {
-            audioManager.Play("LockedDoor");
         }
         yield return null;
     }
