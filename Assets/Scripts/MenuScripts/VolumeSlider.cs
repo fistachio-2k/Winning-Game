7b1031bf39629e009d65e10b6db64e31e45dc9d6
DorGd <37707720+DorGd@users.noreply.github.com>
2021-02-02 10:56:57 +0200

    Resume button + Volume slider collider

---
+++ Assets/Scripts/MenuScripts/VolumeSlider.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class VolumeSlider : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+}
