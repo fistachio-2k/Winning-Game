8a779698eb10c2ca3dd64983b0e8d4fd5f8ccbc5
MiraFinkel <mirafinkel@gmail.com>
2021-02-02 14:31:28 +0200

    Revert "Merge pull request #100 from DorGd/Mira"
    This reverts commit e267f220d1624959d465c1250e4182d0e6affd7c, reversing
    changes made to 95b617a819a021af6fc879723eac6565f6d1bd11.

--- Assets/Scripts/Interactive Objects Scripts/RecipeBox.cs
+++ Assets/Scripts/Interactive Objects Scripts/RecipeBox.cs
@@ -5,60 +5,40 @@ using DG.Tweening;
 
 public class RecipeBox : MonoBehaviour, IInteractable
 {
-    [SerializeField] private GameObject boxLid;
-    [SerializeField] private GameObject boxBody;
-    [SerializeField] private GameObject recipe;
-    [SerializeField] public bool isLocked = true;
+    private bool _isOpen = true;
+    private Vector3 _baseRotation;
+    [SerializeField] private bool _isLocked = false;
     [SerializeField] float duration = 2f;
-    private bool _isOpen = false;
-    private bool _recipeTaken = false;
-    private Vector3 _baseLidRotation;
     private AudioManager audioManager;
 
     void Start()
     {
-        _baseLidRotation = boxLid.transform.rotation.eulerAngles;
+        _baseRotation = transform.rotation.eulerAngles;
         audioManager = FindObjectOfType<AudioManager>();
-        recipe.GetComponent<BoxCollider>().enabled = false;
-        recipe.GetComponent<MeshCollider>().enabled = false;
     }
 
     public void Interact()
     {
-        _recipeTaken = GameEventsManager._instance.isRecipeCollected();
-        Outline outline = boxLid.GetComponent<Outline>();
-        if (outline != null)
-        {
-            outline.enabled = true;
-        }
         StartCoroutine(OpenClose());
     }
 
     IEnumerator OpenClose()
     {
-        if (!isLocked)
+        if (!_isLocked)
         {
-            if (_isOpen && _recipeTaken)
+            _isOpen = !_isOpen;
+            if (_isOpen)
             {
-                boxLid.transform.DORotate(_baseLidRotation, duration);
-                _isOpen = false;
-            }
-            else if(_isOpen && !_recipeTaken)
-            {
-                recipe.GetComponent<Collect>().Interact();
-                _isOpen = true;
+                transform.DORotate(_baseRotation, duration);
             }
             else
             {
-                boxLid.transform.DORotate(_baseLidRotation + (Vector3.up * 90f) + (Vector3.forward * 3f) + (Vector3.right * -3f), duration);
-                if(recipe == null)
-                {
-                    recipe.GetComponent<BoxCollider>().enabled = true;
-                    recipe.GetComponent<MeshCollider>().enabled = true;
-                }
-                yield return new WaitForSeconds(1f);
-                _isOpen = true;
+                transform.DORotate(_baseRotation + (Vector3.up * 90f) + (Vector3.forward * 3f) + (Vector3.right * -3f), duration);
             }
+        }
+        else
+        {
+            audioManager.Play("LockedDoor");
         }
         yield return null;
     }
